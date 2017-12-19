63fec6ceb70a75ceb8f50a2ffe28d18c9c53b1c1
Remmy Clay <remmy.lauren@gmail.com>
2017-12-19 18:53:10 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -25,7 +25,7 @@
 def speak_to_grandma(string)
   if string.upcase != string
     "HUH?! SPEAK UP, SONNY!"
-  elseif string == "I LOVE YOU GRANDMA!"
+  elsif string == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   elsif string == string.upcase
     "NO, NOT SINCE 1938!"
